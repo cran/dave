5f630e103a5ee24bd3a84e6b1a09ce7f79b6c9f7
Otto Wildi <otto.wildi@wsl.ch>
2013-05-07 00:00:00 +0000

    version 1.4

--- R/srank2.R
+++ R/srank2.R
@@ -1,21 +1,25 @@
-srank2 <-
-function(veg,groups,method,y) {
- library(labdsv)
- library(vegan)
- nspecies<- ncol(veg)
- sp.names<- names(veg)
+srank2<- function(veg,groups,method,y) {
+# library(labdsv)
+# library(vegan)
+	nspecies<- ncol(veg)
+	nreleves<- nrow(veg)
+	sp.names<- names(veg)
+# Checking proper classification
+	if(nreleves != length(groups)) {
+		stop("Classification in 'groups' does not accord with data frame used!")
+	}
 # ff is either indval or F, pp is p, list with both in outputlist
- ff<- rep(0,nspecies)
- pp<- rep(0,nspecies)
- veg<- as.matrix(veg^y)
- if (method == "indval") {
-    cat(method,"\n")
-    o.ind<- indval(veg,groups)
-    ff<- o.ind$indcls
-    pp<- o.ind$pval
-    ordered.species<- sp.names[order(-ff)]
-    ordered.ff<- ff[order(-ff)]
-    ordered.pp<- pp[order(-ff)]
+	ff<- rep(0,nspecies)
+	pp<- rep(0,nspecies)
+	veg<- as.matrix(veg^y)
+	if (method == "indval") {
+		cat(method,"\n")
+		o.ind<- indval(veg,groups)
+		ff<- o.ind$indcls
+		pp<- o.ind$pval
+		ordered.species<- sp.names[order(-ff)]
+		ordered.ff<- ff[order(-ff)]
+		ordered.pp<- pp[order(-ff)]
 #   output for Table 7.1 in Latex
 #    for (i in 1:nspecies) {
 #    cat(sprintf("%6.0f %1s %6.0f %1s %-30s %1s %10.3f %1s  %10.3g",i,"&",order(-ff)[i],"&",ordered.species[i],"&",ordered.ff[i],"&",ordered.pp[i]))
@@ -23,26 +27,26 @@ function(veg,groups,method,y) {
 #    }
 #    end Latex
 #    outputlist
-     Fvalue<- NULL
-     indval<- ff[order(-ff)]
- }
-
-  if (method == "jancey") {
+		Fvalue<- NULL
+		indval<- ff[order(-ff)]
+	}
+	
+	if (method == "jancey") {
 # aov, ff is F, pp is p, list with both in jancey.tab
- ff<- rep(0,nspecies)
- pp<- rep(0,nspecies)
-   for (i in 1:nspecies) {
-      onespecies<- as.matrix(veg[,i])
-      model1<- aov(onespecies~groups)
-      model2<- anova(model1)
-      ff[i]<- model2[[4]][[1]]
-      pp[i]<- model2[[5]][[1]]
+		ff<- rep(0,nspecies)
+		pp<- rep(0,nspecies)
+		for (i in 1:nspecies) {
+			onespecies<- as.matrix(veg[,i])
+			model1<- aov(onespecies~groups)
+			model2<- anova(model1)
+			ff[i]<- model2[[4]][[1]]
+			pp[i]<- model2[[5]][[1]]
 #     jancey.tab[i,]<- c(names(veg)[i],ff[i],pp[i])
-   }
+		}
 # printed output
-  ordered.species<- names(veg)[order(-ff)]
-  ordered.ff<- ff[order(-ff)]
-  ordered.pp<- pp[order(-ff)]
+		ordered.species<- names(veg)[order(-ff)]
+		ordered.ff<- ff[order(-ff)]
+		ordered.pp<- pp[order(-ff)]
 # output for Table 7.1 in Latex
 #  for (i in 1:nspecies) {
 #  cat(sprintf("%6.0f %1s %6.0f %1s %-30s %1s %10.3f %1s  %10.3g",i,"&",order(-ff)[i],"&",ordered.species[i],"&",ordered.ff[i],"&",ordered.pp[i]))
@@ -50,8 +54,8 @@ function(veg,groups,method,y) {
 #  }
 # end Latex
 #  }
-   indval<- NULL
-   Fvalue<- ff[order(-ff)]
-  }
-  o.srank<- list(method=method,rank=seq(1,nspecies,1),species.no=order(-ff),species=sp.names[order(-ff)],Indval=indval,F_value=Fvalue,error.probability=pp[order(-ff)])
- }
+		indval<- NULL
+		Fvalue<- ff[order(-ff)]
+	}
+	o.srank<- list(method=method,rank=seq(1,nspecies,1),species.no=order(-ff),species=sp.names[order(-ff)],Indval=indval,F_value=Fvalue,error.probability=pp[order(-ff)])
+}
